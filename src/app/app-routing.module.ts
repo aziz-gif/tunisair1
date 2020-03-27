5b537327f17a7cec7ca064000e5fbf5bf785f616
unknown <aziz.seyah@yahoo.fr>
2020-03-27 17:29:45 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { AgenceComponent } from './agence/agence.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component:HomeComponent
+
+},
+{
+  path:'actualite',
+  component:ActualiteComponent
+},
+{
+  path:'agence',
+  component:AgenceComponent
+},
+{
+  path:'connexion',
+  component:ConnexionComponent
+
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { AgenceComponent } from './agence/agence.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ActualiteComponent,
+    AgenceComponent,
+    NavbarComponent,
+    FooterComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
